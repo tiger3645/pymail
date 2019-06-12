856c96a04de63b37e833e032fb03a4669c5a26f6
Emilio Mendez <emiliomendezcipriani@gmail.com>
2019-06-12 09:10:20 -0300

    Adds utf-8 codification

--- pymail.py
+++ pymail.py
@@ -1,20 +1,41 @@
+from urllib.request import urlopen
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import smtplib
 
-ADDRESS = ''
-PASSWORD = ''
-RECIPIENT = ''
-MESSAGGE = ''
-HOST = ''
-PORT = 000
+def mail():
+    ADDRESS = ''
+    PASSWORD = ''
+    RECIPIENT = ''
+    SUBJECT = ''
+    MESSAGGE = ''
+    HOST = ''
+    PORT = 000
 
-content= MESSAGGE
-mail=smtplib.SMTP(HOST, PORT)
-mail.ehlo()
-mail.starttls()
-mail.login(ADDRESS, PASSWORD)
+    messagge = MIMEMultipart('alternative')
+    messagge.set_charset('utf8')
+    messagge['FROM'] = ADDRESS
+    messagge['To'] = RECIPIENT
+    messagge['Subject'] = SUBJECT
 
-header='To:' + RECIPIENT + '\nFrom:' + ADDRESS + '\n' + 'Subject:' + '' + '\n'
-content=header+content
+    BODY = MIMEText(MESSAGGE.encode('utf-8'), 'html', 'UTF-8')
 
-mail.sendmail(ADDRESS, ADDRESS, content)
-mail.close
+    messagge.attach(BODY)
+
+    mail =smtplib.SMTP(HOST, PORT)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(ADDRESS, PASSWORD)
+    
+    mail.sendmail(ADDRESS, ADDRESS, messagge.as_string())
+    mail.close
+
+def main():
+    while True:
+        if urlopen('https://www.google.com/', timeout=10):
+            break
+    mail()
+
+
+if __name__ == "__main__":
+    main()
